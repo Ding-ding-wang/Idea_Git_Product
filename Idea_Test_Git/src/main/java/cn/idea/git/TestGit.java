d804818891a95be2eb87ab6687bec758a0d44cc2
wangbo_glb <1741633615_glb@qq.com>
2021-11-09 08:32:07 +0800

    idea test git 版本3 update

--- Idea_Test_Git/src/main/java/cn/idea/git/TestGit.java
+++ Idea_Test_Git/src/main/java/cn/idea/git/TestGit.java
@@ -5,5 +5,6 @@ public class TestGit {
     public static void main(String[] args) {
         System.out.println("idea test git...");
         System.out.println("idea test git 版本2...");
+        System.out.println("idea test git 版本3...");
     }
 }
